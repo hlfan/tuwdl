50fc44b539b01a01ef07309c7eff7854396465c8
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2023-10-09 19:58:25 +0200

    Refactor webhook script with Promise.all

--- netlify/functions/dl-webhook.js
+++ netlify/functions/dl-webhook.js
@@ -1,10 +1,9 @@
 exports.handler = async function(event, context) {
-	let list = "", row, cells, time, line;
-	let r=process.env.TUWDL_GSHEETS_JSON;
-	r=await(await fetch(r)).text();
-	for (row of JSON.parse(r.split('setResponse')[1].slice(1, -2)).table.rows) {
-		cells = row.c;
-		time = cells[0]?.v ? eval("new " + cells[0]?.v) : new Date(0);
+	let things2get = [{v:'GSHEETS_JSON',m:'text'},{v:'DISCORD_WEBHOOK_URL',m:'json'}],
+		[data, message] = await Promise.all(things2get.map(t=>fetch(process.env['TUWDL_'+t.v]).then(a=>a[t.m]()))),
+		list="";
+	for (let row of JSON.parse(data.match(/\(({.+})\)/)[1]).table.rows) {
+		time = row.c[0]?.v ? eval("new " + row.c[0]?.v) : new Date(0);
 		line = new Date(time.toLocaleString('en-US',{timeZone:'Europe/Vienna'}));
 		line = `+0${(line.getTime()-time.getTime())/36e5}:00`;
 		time = new Date(time.toISOString().slice(0,-1)+line);
@@ -13,22 +12,19 @@ exports.handler = async function(event, context) {
 		time -= Date.now();
 		if (time < 0) continue;
 		time = (time < 6048e5) ? line : "";
-		line = '\n' + cells[13]?.v.replace(/{d}/g, cells[0].f.replace(/\. /g, ' '));
-		row = cells[1]?.v && eval("new " + cells[1]?.v) > new Date;
+		line = '\n' + row.c[13]?.v.replace(/{d}/g, row.c[0].f.replace(/\. /g, ' '));
+		row = row.c[1]?.v && eval("new " + row.c[1]?.v) > new Date;
 		line = line.replace(/\((\w)\b/g, `(${process.env.URL}/$1`);
 		if(!row) line = line.replace(/__/g, '___');
-		if (cells[5]?.v != 'TRUE') line = line.replace(/__/g, '');
+		if (row.c[5]?.v != 'TRUE') line = line.replace(/__/g, '');
 		row = list + line + time;
 		if (row.length < 2e3) list = row;
 	}
 	if(list.includes('#NAME'))return{statusCode:503};
-	r=process.env.TUWDL_DISCORD_WEBHOOK_URL;
-	list=({content:list.replace(/@/g, '@ ').replace(/  /g, ' ').trim()});
-	line=(await(await fetch(process.env.TUWDL_DISCORD_WEBHOOK_URL)).json()).content;
-	if(list.content===line)return{statusCode:204};
-	line=({body:JSON.stringify(list),method:"PATCH"});
+	let content=list.replace(/@/g, '@ ').replace(/  /g, ' ').trim();
+	if(message.content===content){console.log('no change necessary');return{statusCode:204}};
+	line=({body:JSON.stringify({content}),method:"PATCH"});
 	line.headers=({"content-type":"application/json"});
-	console.log("Received event body:", event.body);
 	console.log(await(await fetch(r,line)).json());
     return{statusCode:204}
 }
