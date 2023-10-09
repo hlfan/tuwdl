330b3a9b84137b0df775a0ff3cc39404b7188022
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2023-10-09 20:03:44 +0200

    Divide colliding vars

--- netlify/functions/dl-webhook.js
+++ netlify/functions/dl-webhook.js
@@ -13,12 +13,12 @@ exports.handler = async function(event, context) {
 		if (time < 0) continue;
 		time = (time < 6048e5) ? line : "";
 		line = '\n' + row.c[13]?.v.replace(/{d}/g, row.c[0].f.replace(/\. /g, ' '));
-		row = row.c[1]?.v && eval("new " + row.c[1]?.v) > new Date;
+		from = row.c[1]?.v && eval("new " + row.c[1]?.v) > new Date;
 		line = line.replace(/\((\w)\b/g, `(${process.env.URL}/$1`);
-		if(!row) line = line.replace(/__/g, '___');
+		if(!from) line = line.replace(/__/g, '___');
 		if (row.c[5]?.v != 'TRUE') line = line.replace(/__/g, '');
-		row = list + line + time;
-		if (row.length < 2e3) list = row;
+		trow = list + line + time;
+		if (trow.length < 2e3) list = trow;
 	}
 	if(list.includes('#NAME'))return{statusCode:503};
 	let content=list.replace(/@/g, '@ ').replace(/  /g, ' ').trim();
