987e671da4379539408541c9de7b1761da44282b
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2023-10-09 09:35:53 +0200

    Start webhook script migration

---
+++ netlify/functions/dl-webhook.js
@@ -0,0 +1,8 @@
+exports.handler = async function(event, context) {
+    console.log("Received event:", event);
+	r=TUWDL_DISCORD_WEBHOOK_URL;
+	line=({body:JSON.stringify({content:`Test: ${new Date()}`}),method:"PATCH"});
+	line.headers=({"content-type":"application/json"});
+	console.log(await(await fetch(r,line)).json());
+    return{statusCode:204}
+}
