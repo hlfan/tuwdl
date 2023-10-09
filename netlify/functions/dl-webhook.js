61f0832577d3c12b1e59c613da13d0e992e98f82
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2023-10-09 09:54:56 +0200

    Fix env var ref

--- netlify/functions/dl-webhook.js
+++ netlify/functions/dl-webhook.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
     console.log("Received event:", event);
-	r=TUWDL_DISCORD_WEBHOOK_URL;
+	r=process.env.TUWDL_DISCORD_WEBHOOK_URL;
 	line=({body:JSON.stringify({content:`Test: ${new Date()}`}),method:"PATCH"});
 	line.headers=({"content-type":"application/json"});
 	console.log(await(await fetch(r,line)).json());
